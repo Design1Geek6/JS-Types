2eff14dd4ed452aa004ceeb1977b1d87302330d9
Design1Geek6 <Design.Geek@outlook.com>
2018-03-27 17:52:28 -0600

    Java Script type Assignment

---
+++ js-types.js
@@ -0,0 +1,13 @@
+// For this assignment you'll need to concatenate (add together) your first name and last name into a string.
+// Assign the result of the string concatenation to a variable: name 
+ 
+ 
+// Add two numbers together, and assign the result in a variable: calculation 
+// Add a number and a string together. Store the result in a variable: unexpected 
+// What happened?  Why? 
+
+first = 'Tyler ';
+middle = 'Coder ';
+last = 'Morrill';
+
+console.log (name = first + middle + last);
